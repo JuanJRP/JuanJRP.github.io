aea67268a5f13c3661932e91586d525045ca3a40
JuanJRP <juanjoserestrepopabon@gmail.com>
2022-05-28 16:19:18 -0500

    Finalizado

--- PHP/Sing-up.php
+++ PHP/Sing-up.php
@@ -8,5 +8,5 @@ $passwd = $_POST["password"];
 $conectar = conn();
 $sql = "INSERT INTO `usuarios` (`nombre1`, `nombre2`, `correo`, `user`, `passwd`) VALUES ('$nombre1', '$nombre2', '$email', '$user', '$passwd');";
 $resul = mysqli_query($conectar,$sql) or trigger_error("query failed! SQL-ERROR: ".mysqli_errno($conectar));
-echo $sql;
+header('../index.html');
 ?>
--- PHP/login.php
+++ PHP/login.php
@@ -5,34 +5,16 @@ $passwd = $_POST["passwd"];
 $conectar = conn();
 $sql = "SELECT * FROM `usuarios` WHERE user = '$user' AND passwd = '$passwd'";
 $resul = mysqli_query($conectar,$sql);
-$rows = mysqli_fetch_row($resul);
-if($rows > 0){
-    echo "bien";
+$rowsuser = mysqli_fetch_row($resul);
+$sql = "SELECT * FROM `admins` WHERE user = '$user' AND passwd = '$passwd'";
+$resul = mysqli_query($conectar,$sql);
+$rowsadmins = mysqli_fetch_row($resul);
+
+if($rowsadmins > 0){
+    header('location: ../Pages/admins.html');
+}else if($rowsuser > 0){
+    header('location: ../Pages/welcome.html');
 }else{
-    ?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Taken</title>
-        <link rel="stylesheet" href="../CSS/Styles-index.css">
-    </head>
-    <body>
-        <div class="login-box">
-            <img class="Avatar" src="../Images/Logo.png" alt="Logo">
-            <h1>erorr</h1>
-            <form action="../PHP/login.php" method="post">
-                <label for="username">Username</label>
-                <input type="text" placeholder="Enter Username" name="user" required>
-                <label for="Password">Password</label>
-                <input type="password" placeholder="Enter Password" name="passwd" required>
-                <input type="submit" value="Log In">
-                <a href="Pages/Lost-Passwd.html">Lost Your Password?</a><br>
-                <a href="Pages/Sing-up.html">Don't have an account?</a>
-            </form>
-        </div>
-    </body>
-</html>
-    <?php
+    echo 'error';
 }
 ?>
